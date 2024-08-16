48e487c0aaee0e91b49e61ff4444629bc1de4bff
Rasmus Steen <rasmus@steen.ninja>
2024-08-16 21:18:24 +0200

    fixing movement and adding rotation system

--- src/main.rs
+++ src/main.rs
@@ -11,7 +11,7 @@ fn main() {
     app.init_resource::<CursorCoords>();
 
     app.add_systems(Startup, setup);
-    app.add_systems(Update, (cursor_to_world, mouse_input_system).chain());
+    app.add_systems(Update, (cursor_to_world, mouse_input_system, debug_text_system).chain());
     app.add_systems(FixedUpdate, (rotation_system, movement_system).chain());
     app.run();
 }
@@ -21,7 +21,11 @@ struct PlayerEntity {
     movement_speed: f32,
     rotation_speed: f32,
     target: Vec2,
+    facing: f32,
 }
+
+#[derive(Component)]
+struct DebugText;
 
 fn setup(
     mut commands: Commands,
@@ -49,9 +53,10 @@ fn setup(
             ..default()
         },
         PlayerEntity {
-            rotation_speed: f32::to_radians(10.),
-            movement_speed: 1.,
+            rotation_speed: f32::to_radians(45.),
+            movement_speed: 3.,
             target: Vec2::new(0.0, 0.0),
+            facing: 0.0,
         },
     ));
 
@@ -61,7 +66,7 @@ fn setup(
             shadows_enabled: true,
             ..default()
         },
-        transform: Transform::from_xyz(4.0, 8.0, 4.0),
+        transform: Transform::from_xyz(4.0, 8.0, 8.0),
         ..default()
     });
 
@@ -73,23 +78,107 @@ fn setup(
             ..default()
         },
     ));
+
+
+
+    // debug text
+    commands.spawn((
+        TextBundle::from_sections([
+            TextSection::new(
+                "facing: \n",
+                TextStyle {
+                    font_size: 12.0,
+                    ..default()
+                },
+            ),
+            TextSection::new(
+                "rotation: \n",
+                TextStyle {
+                    font_size: 12.0,
+                    ..default()
+                },
+            ),
+            TextSection::new(
+                "target: \n",
+                TextStyle {
+                    font_size: 12.0,
+                    ..default()
+                },
+            ),
+            TextSection::new(
+                "position: \n",
+                TextStyle {
+                    font_size: 12.0,
+                    ..default()
+                },
+            ),
+        ])
+        .with_text_justify(JustifyText::Left)
+        // Set the style of the TextBundle itself.
+        .with_style(Style {
+            position_type: PositionType::Absolute,
+            top: Val::Px(5.0),
+            left: Val::Px(5.0),
+            ..default()
+        }),
+        DebugText,
+    ));
 }
 
 fn rotation_system(time: Res<Time>, mut query: Query<(&PlayerEntity, &mut Transform)>) {
     let (player, mut transform) = query.single_mut();
-    transform.rotate_z(player.rotation_speed * time.delta_seconds());
+    let remainder = transform.rotation.z - player.facing;
+
+    if remainder == 0.0 {
+        return;
+    }
+
+    let facing = player.facing;
+    println!("{facing}");
+    let rotation = transform.rotation.z;
+    println!("{rotation}");
+    let remainder_abs = remainder.abs();
+    println!("{remainder_abs}");
+    let rotation_add = player.rotation_speed * time.delta_seconds();
+    println!("{rotation_add}");
+    if rotation_add > remainder_abs {
+        transform.rotate_local_z(remainder);
+        return;
+    }
+    
+    let mut normalized = 1.0;
+    if remainder < 0.0 {
+        normalized = -1.0;
+    }
+
+    transform.rotate_local_z(rotation_add * normalized);
 }
 
 fn movement_system(time: Res<Time>, mut query: Query<(&PlayerEntity, &mut Transform)>) {
     let (player, mut transform) = query.single_mut();
     let current_position = Vec2::new(transform.translation.x, transform.translation.y);
-    println!("curr: {}:{}", current_position.x, current_position.y);
-    let direction = (player.target - current_position).normalize();
-    println!("dir: {}:{}", direction.x, direction.y);
+    let remainder = player.target - current_position;
+
+    if remainder.length() == 0.0 {
+        return;
+    }
+
+    let direction = remainder.normalize();
     let movement = direction * (player.movement_speed * time.delta_seconds());
-    println!("{}:{}", movement.x, movement.y);
-    transform.translation.x += movement.x;
-    transform.translation.y += movement.y;
+    let abs_rem = remainder.abs();
+    let abs_move = movement.abs();
+
+    if abs_rem.x.abs() < abs_move.x.abs() {
+        transform.translation.x += remainder.x
+    } else {
+        transform.translation.x += movement.x;
+    }
+
+    if abs_rem.y.abs() < abs_move.y.abs() {
+        transform.translation.y += remainder.y
+    } else {
+        transform.translation.y += movement.y;
+    }
 }
 
 fn mouse_input_system(
@@ -98,7 +187,29 @@ fn mouse_input_system(
     mut query: Query<(&mut PlayerEntity, &Transform)>,
 ) {
     if mouse_button_input.just_pressed(MouseButton::Right) {
-        let (mut player, _) = query.single_mut();
+        let (mut player, transform) = query.single_mut();
         player.target = Vec2::new(cursor_coords.global.x, cursor_coords.global.y);
+        let direction = transform.translation - cursor_coords.global;
+        player.facing = direction.y.atan2(direction.x);
     }
+}
+
+fn debug_text_system(
+    player_query: Query<(&PlayerEntity, &Transform)>,
+    mut query: Query<&mut Text, With<DebugText>>
+) {
+    let mut text = query.single_mut();
+    let (player, transform) = player_query.single();
+
+    let facing = player.facing;
+    let rotation = transform.rotation.z;
+    let target_x = player.target.x;
+    let target_y = player.target.y;
+    let position_x = transform.translation.x;
+    let position_y = transform.translation.y;
+
+    text.sections[0].value = format!("facing: {facing}\n");
+    text.sections[1].value = format!("rotation: {rotation}\n");
+    text.sections[2].value = format!("target: (x: {target_x}, y: {target_y})\n");
+    text.sections[3].value = format!("position: (x: {position_x}, y: {position_y})\n");
 }
